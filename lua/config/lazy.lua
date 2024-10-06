2beb5bd1a628750c765553b1d409518e750a5b43
JanLubojacky <jenda.lubojacky@gmail.com>
2024-10-06 21:07:53 +0200

    add new formatters

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,6 +12,7 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,6 +3,9 @@ return {
   opts = function()
     local opts = {
       formatters_by_ft = {
+        lua = {"stylua"},
+        c = {"clang-format"},
+        cpp = {"clang-format"},
         python = {"ruff_fix", "ruff_format"},
         rust = {"rustfmt"},
       }
